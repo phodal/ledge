73ea6ef8227817263c0ab9e339db3af272588194
Phodal Huang <h@phodal.com>
2020-03-11 15:09:05 +0800

    styles: align styles

--- src/app/maturity/maturity.component.ts
+++ src/app/maturity/maturity.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import marked from 'marked';
+import MarkdownHelper from '../shared/components/model/markdown.helper';
+import {MarkdownTaskItemService} from '../shared/components/markdown-radar-chart/markdown-task-item.service';
 
 @Component({
   selector: 'app-maturity',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./maturity.component.scss']
 })
 export class MaturityComponent implements OnInit {
+  private textValue = `
+ -  [ ] 配置管理:3
+ -  [ ] 持续与持续集成:3
+ -  [ ] 测试管理:3
+ -  [ ] 部署与发布管理:4
+ -  [ ] 环境管理:4
+ -  [ ] 数据管理:4
+ -  [ ] 数据变更管理:3
+ -  [ ] 度量与反馈:3
 
-  constructor() { }
+  `;
+  private tasks: any;
 
-  ngOnInit(): void {
+  constructor(private markdownTaskItemService: MarkdownTaskItemService) {
+
   }
 
+  ngOnInit(): void {
+    const tokens = marked.lexer(this.textValue);
+    this.tasks = MarkdownHelper.markdownToJSON(tokens, this.tasks);
+    this.markdownTaskItemService.setTasks(this.tasks);
+  }
+
+  updateModel($event: any) {
+
+  }
 }
