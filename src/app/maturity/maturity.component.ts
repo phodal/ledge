31aeadaf1e41f5edf633dee11cb9f812c5c94ba5
Phodal Huang <h@phodal.com>
2020-03-11 15:34:25 +0800

    feat: make it change works

--- src/app/maturity/maturity.component.ts
+++ src/app/maturity/maturity.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import marked from 'marked';
 import MarkdownHelper from '../shared/components/model/markdown.helper';
 import {MarkdownTaskItemService} from '../shared/components/markdown-radar-chart/markdown-task-item.service';
+import {MarkdownListModel} from '../shared/components/model/markdown.model';
 
 @Component({
   selector: 'app-maturity',
@@ -33,6 +34,9 @@ export class MaturityComponent implements OnInit {
 
   `;
   private tasks: any;
+  private tempValue: string;
+  private taskIndex: number;
+  private indexString: string;
 
   constructor(private markdownTaskItemService: MarkdownTaskItemService) {
 
@@ -44,7 +48,72 @@ export class MaturityComponent implements OnInit {
     this.markdownTaskItemService.setTasks(this.tasks);
   }
 
-  updateModel($event: any) {
+  updateValue(value) {
+    this.textValue = value;
+    const tokens = marked.lexer(this.textValue);
+    this.tasks = MarkdownHelper.markdownToJSON(tokens, this.tasks);
+    this.markdownTaskItemService.setTasks(this.tasks);
+  }
 
+  updateModel($event: any) {
+    this.taskIndex = 0;
+    this.tempValue = '';
+    this.indexString = '';
+
+    this.taskToMarkdownList($event);
+    this.tempValue = this.tempValue.replace(/\$[A-Za-z0-9_-]{7,14}/g, '');
+    this.updateValue(this.tempValue);
+  }
+
+  taskToMarkdownList(tasks: any, hasChildren = false) {
+    this.taskIndex++;
+
+    for (const task of tasks) {
+      if (task.item) {
+        const item = task.item as MarkdownListModel;
+        if (this.taskIndex > 1) {
+          this.indexString = '  '.repeat(this.taskIndex);
+        }
+        this.tempValue += this.indexString + ` -`;
+        if (item.id) {
+          this.tempValue += ` $${item.id}`;
+        }
+
+        if (item.completed) {
+          this.tempValue += ' [x]';
+        } else {
+          this.tempValue += ' [ ]';
+        }
+
+        if (item.priority) {
+          this.tempValue += ` (${item.priority})`;
+        }
+        if (item.startDate) {
+          this.tempValue += ` ${item.startDate}`;
+        }
+        if (item.endDate) {
+          this.tempValue += ` ${item.endDate}`;
+        }
+        this.tempValue += ` ${item.text}`;
+        if (item.tag) {
+          for (const tag of item.tag) {
+            this.tempValue += ` +${tag}`;
+          }
+        }
+        if (item.context) {
+          this.tempValue += ` @${item.context}`;
+        }
+        this.tempValue += `\n`;
+        this.indexString = '';
+      }
+
+      if (task.childrens) {
+        this.taskToMarkdownList(task.childrens, hasChildren);
+      }
+    }
+
+    if (!hasChildren) {
+      this.taskIndex--;
+    }
   }
 }
