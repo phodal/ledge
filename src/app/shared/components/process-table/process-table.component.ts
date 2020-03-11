c3f08fed95cc2e27e9ab2c0baf18eb5ad58e436e
Phodal Huang <h@phodal.com>
2020-03-11 21:12:56 +0800

    feat: make basic view

--- src/app/shared/components/process-table/process-table.component.ts
+++ src/app/shared/components/process-table/process-table.component.ts
@@ -1,4 +1,10 @@
 import {Component, Input, OnInit} from '@angular/core';
+import marked from 'marked';
+
+interface ProcessTable {
+  headers: string[];
+  cells: string[][];
+}
 
 @Component({
   selector: 'component-process-table',
@@ -7,10 +13,40 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class ProcessTableComponent implements OnInit {
   @Input()
-  data: [][];
+  tableValue: string;
+  private textValue: string;
+  processTable: ProcessTable = {
+    headers: [],
+    cells: []
+  };
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+    this.textValue = `
+| 项目 / 过程管理 | 配置管理 | 构建  | 测试 / 质量 | 制品 / 部署 | 基础设施 | 沟通协作 | 可视化   |
+|---------------|---------|-------|------------|------------|---------|---------|---------|
+| Jira          | Gitee   | Maven | Junit      | Ubran code | VMWare  | 招呼     | Tableau |
+| Tracker       | Rational ClearCase |  Gradle | Cucumber | Fit2Cloud | OpenShift | 移事通 | Grafana |
+| VP            | CMDB | NPM | JMeter     | B9         | Cloud Foundry | | Kibana |
+| Confluence    |   Firefly    | Ant   | RobotFramework | JFrog Artifactory | | |  Prometheus |
+| ITIL          |    | MSBuild | Protractor | | | | ElasticSearch |
+|               |           |  Docker  | Sonar | | | | X-Pack |
+|               |           |        | BlackDuck | | | | |
+
+    `;
+
+    const tokens = marked.lexer(this.textValue);
+    this.buildData(tokens);
+  }
+
+  buildData(tokens: marked.Token[]) {
+    for (const token of tokens) {
+      if (token.type === 'table') {
+        this.processTable.headers = token.header;
+        this.processTable.cells = token.cells;
+      }
+    }
   }
 }
