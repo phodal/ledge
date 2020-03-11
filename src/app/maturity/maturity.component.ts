02df99f51857f9840b5ee2ff2ac56b5991c78288
Phodal Huang <h@phodal.com>
2020-03-11 15:28:57 +0800

    feat: add data

--- src/app/maturity/maturity.component.ts
+++ src/app/maturity/maturity.component.ts
@@ -10,14 +10,26 @@ import {MarkdownTaskItemService} from '../shared/components/markdown-radar-chart
 })
 export class MaturityComponent implements OnInit {
   private textValue = `
- -  [ ] 配置管理:3
- -  [ ] 持续与持续集成:3
- -  [ ] 测试管理:3
- -  [ ] 部署与发布管理:4
- -  [ ] 环境管理:4
- -  [ ] 数据管理:4
- -  [ ] 数据变更管理:3
- -  [ ] 度量与反馈:3
+ -  [ ] 配置管理: 3
+   - [ ] 版本控制
+   - [ ] 变更管理
+ -  [ ] 持续与持续集成: 3
+   - [ ] 构建实践
+   - [ ] 持续集成
+ -  [ ] 测试管理: 3
+   - [ ] 测试分层策略
+   - [ ] 代码质量管理
+   - [ ] 自动化测试
+ -  [ ] 部署与发布管理: 3
+   - [ ] 部署与发布管理
+   - [ ] 部署流水线
+ -  [ ] 环境管理: 3
+ -  [ ] 数据管理: 3
+   - [ ] 测试数据管理
+   - [ ] 数据变更管理
+ -  [ ] 度量与反馈: 3
+   - [ ] 度量指标
+   - [ ] 度量驱动改进
 
   `;
   private tasks: any;
