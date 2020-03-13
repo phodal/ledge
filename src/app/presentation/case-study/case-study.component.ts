1d30b184044d5e520aa037e060f763b439e3adc9
Phodal Huang <h@phodal.com>
2020-03-13 20:12:24 +0800

    feat: add record last case study

--- src/app/presentation/case-study/case-study.component.ts
+++ src/app/presentation/case-study/case-study.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Title} from '@angular/platform-browser';
+import {StorageMap} from '@ngx-pwa/local-storage';
 
 @Component({
   selector: 'app-case-study',
@@ -13,18 +14,31 @@ export class CaseStudyComponent implements OnInit {
     {displayName: '招商银行', source: 'cmb'},
     {displayName: 'HP', source: 'hp'},
   ];
-  src = `assets/docs/casestudies/meituan.md`;
   currentSource = 'meituan';
+  src = this.buildSrc(this.currentSource);
 
-  constructor(title: Title) {
+  constructor(title: Title, private storage: StorageMap) {
     title.setTitle('DevOps 学习平台 Ledge - 案例学习');
+    this.storage.get('casestudy.last').subscribe((value: string) => {
+      if (!!value) {
+        this.currentSource = value;
+        this.src = this.buildSrc(this.currentSource);
+      }
+    });
   }
 
   ngOnInit(): void {
+
   }
 
   clickCase(source: string) {
-    this.src = `assets/docs/casestudies/${source}.md`;
+    this.src = this.buildSrc(source);
     this.currentSource = source;
+
+    this.storage.set('casestudy.last', source).subscribe();
+  }
+
+  private buildSrc(source: string) {
+    return `assets/docs/casestudies/${source}.md`;
   }
 }
