2860dea52baa7b166be26e2272e6ff54ffaaba57
Phodal Huang <h@phodal.com>
2020-03-16 16:01:26 +0800

    feat: add url router

--- src/app/presentation/case-study/case-study.component.ts
+++ src/app/presentation/case-study/case-study.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Title} from '@angular/platform-browser';
 import {StorageMap} from '@ngx-pwa/local-storage';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 
 @Component({
   selector: 'app-case-study',
@@ -24,18 +25,25 @@ export class CaseStudyComponent implements OnInit {
   currentSource = 'meituan';
   src = this.buildSrc(this.currentSource);
 
-  constructor(title: Title, private storage: StorageMap) {
+  constructor(title: Title, private storage: StorageMap, private activatedRoute: ActivatedRoute, private router: Router) {
     title.setTitle('DevOps 知识平台 Ledge - 案例学习');
-    this.storage.get('casestudy.last').subscribe((value: string) => {
-      if (!!value) {
-        this.currentSource = value;
-        this.src = this.buildSrc(this.currentSource);
-      }
-    });
   }
 
   ngOnInit(): void {
-
+    this.activatedRoute.queryParams.subscribe(params => {
+      const source = params.source;
+      if (source) {
+        this.currentSource = source;
+        this.src = this.buildSrc(this.currentSource);
+      } else {
+        this.storage.get('casestudy.last').subscribe((value: string) => {
+          if (!!value) {
+            this.currentSource = value;
+            this.src = this.buildSrc(this.currentSource);
+          }
+        });
+      }
+    });
   }
 
   clickCase(source: string) {
@@ -43,6 +51,15 @@ export class CaseStudyComponent implements OnInit {
     this.currentSource = source;
 
     this.storage.set('casestudy.last', source).subscribe();
+
+    const queryParams: Params = { source };
+    this.router.navigate(
+      [],
+      {
+        relativeTo: this.activatedRoute,
+        queryParams,
+        queryParamsHandling: 'merge', // remove to replace all query params by provided
+      });
   }
 
   private buildSrc(source: string) {
