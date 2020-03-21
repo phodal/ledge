6a0de4e3d1c2adb551f8c173fbbe0c27c5a558c4
Phodal Huang <h@phodal.com>
2020-03-21 12:51:26 +0800

    refactor: remove old d3

--- src/app/shared/components/markdown-radar-chart/markdown-radar-chart.component.ts
+++ src/app/shared/components/markdown-radar-chart/markdown-radar-chart.component.ts
@@ -1,11 +1,7 @@
 import { AfterViewInit, Component, ElementRef, forwardRef, OnInit, ViewChild } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
-
-import RadarChart from './RadarChart.js';
 import MarkdownHelper from '../model/markdown.helper';
 import { MarkdownListModel } from '../model/markdown.model';
-
-import d3 from 'd3';
 
 @Component({
   selector: 'component-markdown-radar-chart',
@@ -90,61 +86,5 @@ export class MarkdownRadarChartComponent implements OnInit, AfterViewInit, Contr
     }
     this.data = this.taskToData();
 
-    const w = 500;
-    const h = 500;
-
-    let colorscale = d3.scale.category10();
-
-    let LegendOptions = ['Current', 'Future'];
-    let data = this.data;
-
-    // Options for the Radar chart, other than default
-    let mycfg = {
-      width: w,
-      height: h,
-      maxValue: 5,
-      levels: 5,
-      ExtraWidthX: 300
-    };
-
-    RadarChart.draw('#chart', data, mycfg);
-
-    let svg = d3.select('#body')
-      .selectAll('svg')
-      .append('svg')
-      .attr('width', w + 300)
-      .attr('height', h);
-
-    let legend = svg.append('g')
-      .attr('class', 'legend')
-      .attr('height', 100)
-      .attr('width', 200)
-      .attr('transform', 'translate(90,20)');
-
-    legend.selectAll('rect')
-      .data(LegendOptions)
-      .enter()
-      .append('rect')
-      .attr('x', w - 65)
-      .attr('y', (d, i) => i * 20)
-      .attr('width', 10)
-      .attr('height', 10)
-      .style('fill', function(d, i) {
-        return colorscale(i.toString());
-      });
-
-    legend.selectAll('text')
-      .data(LegendOptions)
-      .enter()
-      .append('text')
-      .attr('x', w - 52)
-      .attr('y', function(d, i) {
-        return i * 20 + 9;
-      })
-      .attr('font-size', '11px')
-      .attr('fill', '#737373')
-      .text(function(d) {
-        return d;
-      });
   }
 }
