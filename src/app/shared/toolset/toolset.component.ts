868db582c16e771e6b4fefcdb39c1a9821b1a2c4
Phodal Huang <h@phodal.com>
2020-03-26 17:01:36 +0800

    feat: design toolset model

--- src/app/shared/toolset/toolset.component.ts
+++ src/app/shared/toolset/toolset.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-toolset',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./toolset.component.scss']
 })
 export class ToolsetComponent implements OnInit {
+
+  @Input()
+  option: ToolsetOption;
 
   constructor() { }
 
---
+++ src/app/shared/toolset/toolset.model.ts
@@ -0,0 +1,6 @@
+interface ToolsetOption {
+  id: string;
+  type: string; // slider
+  offsetHeight: string;
+  height: string;
+}
