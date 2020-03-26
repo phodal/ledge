5d08af28648350292ec39839018d5562dd07a603
Phodal Huang <h@phodal.com>
2020-03-26 16:21:32 +0800

    revert: revert tooset

--- src/app/shared/toolset/toolset.component.ts
+++ src/app/shared/toolset/toolset.component.ts
@@ -1,5 +1,4 @@
-import '@angular/platform-browser-dynamic';
-import { AfterViewInit, Compiler, Component, Injector, NgModule, NgModuleRef, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 
 @Component({
   selector: 'app-toolset',
@@ -11,27 +10,11 @@ import { AfterViewInit, Compiler, Component, Injector, NgModule, NgModuleRef, On
 export class ToolsetComponent implements AfterViewInit, OnInit {
   @ViewChild('dynamicTemplate', {read: ViewContainerRef}) dynamicTemplate;
 
-  constructor(private compiler: Compiler,
-              private injector: Injector,
-              private moduleRef: NgModuleRef<any>) {
+  constructor() {
   }
 
   ngAfterViewInit() {
-    const tmpCmp = Component({
-      moduleId: '',
-      template: '<div>hello</div>'
-    })(class {
-    });
-    const tmpModule = NgModule({declarations: [tmpCmp]})(class {
-    });
 
-    this.compiler.compileModuleAndAllComponentsAsync(tmpModule)
-      .then((factories) => {
-        const f = factories.componentFactories[0];
-        const cmpRef = f.create(this.injector, [], null, this.moduleRef);
-        cmpRef.instance.name = 'dynamic';
-        this.dynamicTemplate.insert(cmpRef.hostView);
-      });
   }
 
   ngOnInit(): void {
