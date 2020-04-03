b94ce322b1215af238a82d68817e760aaa11b195
Phodal Huang <h@phodal.com>
2020-04-03 18:06:08 +0800

    fix: fix manual behavior error issue

--- src/app/presentation/manual/manual.component.ts
+++ src/app/presentation/manual/manual.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import * as mdData from 'raw-loader!../../../assets/docs/practise.md';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-manual',
   templateUrl: './manual.component.html',
-  styleUrls: ['./manual.component.scss']
+  styleUrls: ['./manual.component.scss'],
 })
 export class ManualComponent implements OnInit {
+  data = mdData.default;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(title: Title) {
+    title.setTitle('DevOps 知识平台 Ledge - 操作手册');
   }
 
+  ngOnInit(): void {}
 }
