8e8fe125dfa16ad04baaa59d3eb7c289d59f36c9
Phodal Huang <h@phodal.com>
2020-04-03 09:34:07 +0800

    feat: <design> add reset button for #26

--- src/app/presentation/design/path/path.component.ts
+++ src/app/presentation/design/path/path.component.ts
@@ -16,6 +16,63 @@ interface Item {
   textColor: string;
 }
 
+const originPipeLine = [
+  {
+    id: 1,
+    title: 'Process',
+    items: [
+      // tslint:disable-next-line:max-line-length
+      'Commit Code',
+      'PUSH Hooks',
+      'RUN CI',
+      'Deploy Dev',
+      'E2E Test',
+      'Manual Test',
+      'Deploy UAT',
+      'Manual Test',
+      'Go-Live Apply',
+      'Go-Live',
+    ],
+    backgroundColor: '#00a300',
+    textColor: '#ffffff',
+  },
+  {
+    id: 2,
+    title: 'People',
+    items: [''],
+    backgroundColor: '#ff0097',
+    textColor: '#ffffff',
+  },
+  {
+    id: 3,
+    title: 'Tooling',
+    items: ['Git & GitHub', 'Git', 'Jenkins', '', '', '', '', ''],
+    backgroundColor: '#99b433',
+    textColor: '#ffffff',
+  },
+  {
+    id: 4,
+    title: 'Artifacts',
+    items: ['Code', '', 'Build Log', '', '', '', '', '', '', ''],
+    backgroundColor: '#1e7145',
+    textColor: '#ffffff',
+  },
+  {
+    id: 5,
+    title: 'Pain',
+    items: [''],
+    backgroundColor: '#00aba9',
+    textColor: '#ffffff',
+  },
+  {
+    id: 6,
+    title: 'Duration',
+    items: [''],
+    backgroundColor: '#ffc40d',
+    textColor: '#ffffff',
+  },
+];
+
 @Component({
   selector: 'component-path',
   templateUrl: './path.component.html',
@@ -23,62 +80,7 @@ interface Item {
 })
 export class PathComponent implements OnInit {
   @ViewChildren('itemElement') itemElements: QueryList<ElementRef>;
-  pipeData = [
-    {
-      id: 1,
-      title: 'Process',
-      items: [
-        // tslint:disable-next-line:max-line-length
-        'Commit Code',
-        'PUSH Hooks',
-        'RUN CI',
-        'Deploy Dev',
-        'E2E Test',
-        'Manual Test',
-        'Deploy UAT',
-        'Manual Test',
-        'Go-Live Apply',
-        'Go-Live',
-      ],
-      backgroundColor: '#00a300',
-      textColor: '#ffffff',
-    },
-    {
-      id: 2,
-      title: 'People',
-      items: [''],
-      backgroundColor: '#ff0097',
-      textColor: '#ffffff',
-    },
-    {
-      id: 3,
-      title: 'Tooling',
-      items: ['Git & GitHub', 'Git', 'Jenkins', '', '', '', '', ''],
-      backgroundColor: '#99b433',
-      textColor: '#ffffff',
-    },
-    {
-      id: 4,
-      title: 'Artifacts',
-      items: ['Code', '', 'Build Log', '', '', '', '', '', '', ''],
-      backgroundColor: '#1e7145',
-      textColor: '#ffffff',
-    },
-    {
-      id: 5,
-      title: 'Pain',
-      items: [''],
-      backgroundColor: '#00aba9',
-      textColor: '#ffffff',
-    },
-    {
-      id: 6,
-      title: 'Duration',
-      items: [''],
-      backgroundColor: '#ffc40d',
-      textColor: '#ffffff',
-    },
-  ];
+  pipeData = originPipeLine;
   maxLength: number;
 
   constructor(private storage: StorageMap, private renderer: Renderer2) {}
@@ -206,5 +208,12 @@ export class PathComponent implements OnInit {
       const element = filterElements[0];
       this.renderer.setProperty(element.nativeElement, 'contentEditable', true);
     }
+  }
+
+  resetAll() {
+    this.pipeData = originPipeLine;
+    this.maxLength = this.getMaxLength(originPipeLine);
+    this.fillDefaultValue();
+    this.storage.set('ledge.path', this.pipeData).subscribe(() => {});
   }
 }
