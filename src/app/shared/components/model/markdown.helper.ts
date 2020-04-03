3da3f238aec3513f71011147d696a249f94516fc
Phodal Huang <h@phodal.com>
2020-04-03 21:54:05 +0800

    refactor: remove old todo compiled

--- src/app/shared/components/model/markdown.helper.ts
+++ src/app/shared/components/model/markdown.helper.ts
@@ -1,25 +1,16 @@
 // tslint:disable-next-line:max-line-length
 // REFS: https://github.com/todotxt/todo.txt-android/blob/614e0b5eb688cae8236f33c64d7e791d1030cf3c/app/src/main/java/com/todotxt/todotxttouch/task/TextSplitter.java
 import shortid from 'shortid';
-import {MarkdownListModel} from './markdown.model';
+import { MarkdownListModel } from './markdown.model';
 
 const COMPLETED_PATTERN = /(\[[x|X]] )(.*)/;
-const COMPLETED_PREPENDED_DATES_PATTERN = /(\d{4}-\d{2}-\d{2}) (\d{4}-\d{2}-\d{2}) (.*)/;
-const SINGLE_DATE_PATTERN = /(\d{4}-\d{2}-\d{2}) (.*)/;
 const ID_PATTERN = /(\s\$([A-Za-z0-9_-]{7,14})) (.*)/;
-const CONTEXT_PATTERN = /(?:^|\s)@(\S*\w)/;
 const TAG_PATTERN = /(?:^|\s)\+(\S*\w)/g;
-const PRIORITY_PATTERN = /\(([A-Z])\) (.*)/;
 
 const MarkdownHelper = {
   todoCompiled(text: any): MarkdownListModel {
     const originText = text;
     let completed = false;
-    let startDate;
-    let endDate;
-    let context;
-    let priority;
-    let tag;
     let id;
 
     TAG_PATTERN.lastIndex = 0;
@@ -36,39 +27,6 @@ const MarkdownHelper = {
       text = completeMatch[2];
     }
 
-    const priorityMatch = PRIORITY_PATTERN.exec(text);
-    if (priorityMatch && priorityMatch.length && priorityMatch.length > 1) {
-      priority = priorityMatch[1];
-      text = priorityMatch[2];
-    }
-
-    const dateMatch = COMPLETED_PREPENDED_DATES_PATTERN.exec(text);
-    if (dateMatch && dateMatch.length && dateMatch.length > 2) {
-      startDate = dateMatch[1];
-      endDate = dateMatch[2];
-      text = dateMatch[3];
-    } else {
-      const singleDateMatch = SINGLE_DATE_PATTERN.exec(text);
-      if (singleDateMatch && singleDateMatch.length && singleDateMatch.length >= 2) {
-        endDate = singleDateMatch[1];
-        text = singleDateMatch[2];
-      }
-    }
-
-    const contextMatch = CONTEXT_PATTERN.exec(text);
-    if (contextMatch && contextMatch.length && contextMatch.length > 1) {
-      context = contextMatch[1];
-      text = text.replace(CONTEXT_PATTERN, '');
-    }
-
-    const tagMatch = TAG_PATTERN.exec(text);
-    if (tagMatch && tagMatch.length && tagMatch.length > 1) {
-      tag = text.match(TAG_PATTERN).map(t => {
-        return t.replace(/(?:^|\s)\+/, '');
-      });
-      text = text.replace(TAG_PATTERN, '');
-    }
-
     if (!id) {
       id = shortid.generate();
     }
@@ -77,12 +35,7 @@ const MarkdownHelper = {
       originText,
       id,
       completed,
-      startDate,
-      endDate,
-      tag,
-      context,
-      priority,
-      text
+      text,
     };
   },
 
@@ -111,10 +64,17 @@ const MarkdownHelper = {
           break;
         }
         case 'text': {
-          if (token.text.includes('[x] ') || token.text.includes('[X] ') || token.text.includes('[ ] ')) {
+          if (
+            token.text.includes('[x] ') ||
+            token.text.includes('[X] ') ||
+            token.text.includes('[ ] ')
+          ) {
             checkString = '';
           }
-          result += JSON.stringify(MarkdownHelper.todoCompiled(checkString + token.text)) + ',';
+          result +=
+            JSON.stringify(
+              MarkdownHelper.todoCompiled(checkString + token.text)
+            ) + ',';
           break;
         }
         case 'list_item_end': {
@@ -177,7 +137,7 @@ const MarkdownHelper = {
     }
 
     return item;
-  }
+  },
 };
 
 export default MarkdownHelper;
