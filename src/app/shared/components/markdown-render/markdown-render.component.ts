db2774b8cb14cb704e47102cc522666964027f89
Phodal Huang <h@phodal.com>
2020-04-03 20:10:20 +0800

    feat(render): #12 make toc works

--- src/app/shared/components/markdown-render/markdown-render.component.ts
+++ src/app/shared/components/markdown-render/markdown-render.component.ts
@@ -53,38 +53,10 @@ export class MarkdownRenderComponent
 
   loading = this.data !== '';
 
-  // marked
-  escapeTest = /[&<>"']/;
-  escapeReplace = /[&<>"']/g;
-  escapeTestNoEncode = /[<>"']|&(?!#?\w+;)/;
-  escapeReplaceNoEncode = /[<>"']|&(?!#?\w+;)/g;
-  escapeReplacements = {
-    '&': '&amp;',
-    '<': '&lt;',
-    '>': '&gt;',
-    '"': '&quot;',
-    "'": '&#39;',
-  };
-  private mindmapIndex = 0;
-  private chartInfos = [];
-  private chartIndex = 0;
-  private chartInstances: ECharts[] = [];
   private toc = [];
   tocStr = '';
   sticky = false;
   windowScrolled = false;
-  private graphvizData = [];
-  private graphvizIndex = 0;
-  private mermaidIndex = 0;
-  private mermaidData = [];
-
-  private echartsIndex = 0;
-  private echartsData = [];
-
-  private webComponentsIndex = 0;
-  private webComponentsData = [];
-
-  private toolsetId = 0;
   toolsets: ToolsetOption[] = [];
 
   private lastTocId: string;
@@ -102,45 +74,20 @@ export class MarkdownRenderComponent
 
   ngOnInit(): void {
     const markedOptions: any = this.markdownService.options;
-    this.markdownService.renderer.image = this.renderImage(markedOptions).bind(
-      this
-    );
-    this.markdownService.renderer.code = this.renderCode(markedOptions).bind(
-      this
-    );
     this.markdownService.renderer.heading = this.renderHeading(
       markedOptions
     ).bind(this);
   }
 
   ngAfterViewInit(): void {
-    if (this.data !== '') {
-      this.endLoading();
-    }
+    this.markdownService.compile(this.data);
+    this.render();
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes.src) {
-      this.mindmapIndex = 0;
-      this.chartIndex = 0;
-      this.webComponentsIndex = 0;
-      this.mermaidIndex = 0;
-      this.toolsetId = 0;
-
-      this.chartInfos = [];
-      this.mermaidData = [];
-      this.graphvizData = [];
-      this.webComponentsData = [];
-      this.toolsets = [];
-    }
-  }
+  ngOnChanges(changes: SimpleChanges): void {}
 
   @HostListener('window:resize', ['$event'])
-  onResize(event) {
-    for (const chartInstance of this.chartInstances) {
-      chartInstance.resize();
-    }
-  }
+  onResize(event) {}
 
   @HostListener('window:scroll', ['$event'])
   handleScroll() {
@@ -201,12 +148,7 @@ export class MarkdownRenderComponent
     }
   }
 
-  endLoading() {
-    this.loading = false;
-    for (const chartInstance of this.chartInstances) {
-      chartInstance.clear();
-    }
-
+  render() {
     const items = this.tocify.tocItems;
     this.tocStr = this.renderToc(items).join('');
     if (this.tocEl && this.tocEl.nativeElement) {
@@ -215,13 +157,6 @@ export class MarkdownRenderComponent
     this.tocify.reset();
 
     this.startSyncMenu();
-
-    setTimeout(() => {
-      this.renderChart();
-      this.renderGraphviz();
-      this.renderEcharts();
-    }, 50);
-
     setTimeout(() => this.gotoHeading(), 500);
   }
 
@@ -245,35 +180,6 @@ export class MarkdownRenderComponent
     });
   }
 
-  private renderToc(items: TocItem[]) {
-    return items.map((item) => {
-      const href = `${this.location.path()}#${item.anchor}`;
-      const link = `<a class="level_${item.level}" id="menu-${item.anchor}" href="${href}" title=${item.text}>${item.text}</a>`;
-      if (item.children) {
-        const childrenItems = this.renderToc(item.children);
-        return `<li>${link}<ul>${childrenItems.join('')}</ul></li>`;
-      } else {
-        return `<li>${link}</li>`;
-      }
-    });
-  }
-
-  private renderImage(markedOptions: any) {
-    return (href: string, title: string, text: string): string => {
-      if (href === null) {
-        return text;
-      }
-      let out = `<img src="${href}" alt="${text}"`;
-      if (title) {
-        out += ` title="${title}"`;
-      }
-
-      out += markedOptions.xhtml ? '/>' : '>';
-      out += `<figcaption>${title}</figcaption>`;
-      return `<figure>${out}</figure>`;
-    };
-  }
-
   private renderHeading(options: any) {
     return (text: string, level: number, raw: string, slugger: Slugger) => {
       const anchor = slugger.slug(raw);
@@ -285,407 +191,16 @@ export class MarkdownRenderComponent
     };
   }
 
-  private renderCode(options: any) {
-    return (code: any, infoStr: any, escaped: any) => {
-      const lang = (infoStr || '').match(/\S*/)[0];
-
-      switch (lang) {
-        case 'process-table':
-          return this.buildTableProcess(code);
-        case 'process-step':
-          return this.buildTableStep(code);
-        case 'mindmap':
-          return this.buildMindmapData(code);
-        case 'radar':
-          return this.buildRadarChartData(code);
-        case 'pyramid':
-          return this.buildPyramidChartData(code);
-        case 'quadrant':
-          return this.buildQuadrantChartData(code);
-        case 'graphviz':
-          return this.buildGraphvizData(code);
-        case 'echarts':
-          return this.buildEchartsData(code);
-        case 'toolset':
-          return this.buildToolsets(code);
-        case 'list-style':
-          return this.buildListStyle(code);
-        default:
-          return this.buildNormalCode(options, code, lang, escaped);
+  private renderToc(items: TocItem[]) {
+    return items.map((item) => {
+      const href = `${this.location.path()}#${item.anchor}`;
+      const link = `<a class="level_${item.level}" id="menu-${item.anchor}" href="${href}" title=${item.text}>${item.text}</a>`;
+      if (item.children) {
+        const childrenItems = this.renderToc(item.children);
+        return `<li>${link}<ul>${childrenItems.join('')}</ul></li>`;
+      } else {
+        return `<li>${link}</li>`;
       }
-    };
-  }
-
-  private buildListStyle(code: any) {
-    const toJson = LedgeMarkdownConverter.toJson(code);
-    const list = toJson.lists[0];
-    let childStr = '';
-    childStr = this.buildListStyleChildren(list, childStr);
-
-    const type = toJson.config.type;
-    const cssClz = `list-style list-style-${type}`;
-
-    return `<div class="${cssClz}">${childStr}</div>`;
-  }
-
-  private buildListStyleChildren(list, childStr: string) {
-    let index = 0;
-    for (const item of list.children as any[]) {
-      let subChildStr = '';
-      let subChildClass = '';
-      if (item.children && item.children.length > 0) {
-        subChildStr = this.buildListStyleChildren(item, subChildStr);
-        subChildClass = 'sub-item';
-      }
-
-      index++;
-      childStr += `<div class="list-style-item item-${index} ${subChildClass}">${item.name}${subChildStr}</div>`;
-    }
-
-    return childStr;
-  }
-
-  private buildNormalCode(options: any, code: any, lang: string, escaped: any) {
-    if (options.highlight) {
-      const out = options.highlight(code, lang);
-      if (out != null && out !== code) {
-        escaped = true;
-        code = out;
-      }
-    }
-
-    if (!lang) {
-      return (
-        '<pre><code>' +
-        (escaped ? code : this.escape(code, true)) +
-        '</code></pre>'
-      );
-    }
-
-    return `<pre>
-    <code class="${options.langPrefix}${this.escape(lang, true)}">${
-      escaped ? code : this.escape(code, true)
-    }</code>
-</pre>`;
-  }
-
-  getEscapeReplacement(ch) {
-    return this.escapeReplacements[ch];
-  }
-
-  escape(html: string, encode: boolean) {
-    if (encode) {
-      if (this.escapeTest.test(html)) {
-        return html.replace(
-          this.escapeReplace,
-          this.getEscapeReplacement.bind(this)
-        );
-      }
-    } else {
-      if (this.escapeTestNoEncode.test(html)) {
-        return html.replace(
-          this.escapeReplaceNoEncode,
-          this.getEscapeReplacement.bind(this)
-        );
-      }
-    }
-
-    return html;
-  }
-
-  private buildProcessHeaderItem(index: number, str: string) {
-    return `
-      <div class="flex-row cell type_${index}">
-        ${str}
-      </div>
-      `;
-  }
-
-  private buildProcessHeader(items: string) {
-    return `<div class="table-container" role="table" aria-label="Destinations">
-    <div class="flex-table header" role="rowgroup">
-      ${items}
-    </div>
-    </div>`;
-  }
-
-  private buildTableProcess(code: any) {
-    let resultStr = '';
-    const { header, cells } = LedgeMarkdownConverter.toJson(code).tables[0];
-    resultStr += this.buildProcessHeader(this.buildHeaderItem(header));
-    const bodyResult = this.buildTableBody(cells);
-
-    resultStr += `<div class="table-space"></div><div class="flex-table row">${bodyResult}</div>`;
-
-    return `<div class="process-table markdown-table">` + resultStr + '</div>';
-  }
-
-  private buildTableBody(cells: any[]) {
-    let bodyResult = '';
-    // tslint:disable-next-line:prefer-for-of
-    for (let index = 0; index < cells.length; index++) {
-      const column = cells[index];
-      let columnStr = '';
-      // tslint:disable-next-line:prefer-for-of
-      for (let j = 0; j < column.length; j++) {
-        columnStr += `<div class="cell">${column[j]}</div>`;
-      }
-
-      bodyResult += `<div class="table-column">${columnStr}</div>`;
-    }
-    return bodyResult;
-  }
-
-  private buildHeaderItem(header: any[]) {
-    let headerStr = '';
-    for (let index = 0; index < header.length; index++) {
-      const head = header[index];
-      headerStr += this.buildProcessHeaderItem(index, head);
-    }
-
-    return headerStr;
-  }
-
-  private buildMindmapData(code: any) {
-    const tokens = marked.lexer(code);
-    let items = [];
-    items = MarkdownHelper.markdownToJSON(tokens, items);
-    const currentMap = {
-      id: 'mindmap-' + this.mindmapIndex,
-      type: 'mindmap',
-      data: items,
-    };
-
-    this.chartInfos.push(currentMap);
-    this.mindmapIndex++;
-    return `<div class="markdown-mindmap ${currentMap.id}"></div>`;
-  }
-
-  private renderChart() {
-    if (this.chartInfos.length === 0) {
-      return;
-    }
-
-    for (const chartInfo of this.chartInfos) {
-      const elements = document.getElementsByClassName(chartInfo.id);
-      if (elements.length < 1) {
-        return;
-      }
-      const chartEl = elements[0];
-      const mychart = echarts.init(chartEl as any);
-      this.chartInstances.push(mychart);
-
-      const newData = this.toTreeData(chartInfo.data);
-      switch (chartInfo.type) {
-        case 'mindmap':
-          mychart.setOption(ChartOptions.buildMindmapOption(newData) as any);
-          break;
-        case 'radarchart':
-          mychart.setOption(ChartOptions.buildRadarChartOption(newData));
-          break;
-        case 'quadrant':
-          mychart.setOption(ChartOptions.buildQuadrantChartOption(newData));
-          break;
-        case 'pyramid':
-          const pyramidLength = newData.children.length;
-          const CHART_MAX_VALUE = 100;
-          for (let i = 0; i < pyramidLength; i++) {
-            newData.children[i].value =
-              (CHART_MAX_VALUE / pyramidLength) * (i + 1);
-          }
-
-          mychart.setOption(
-            ChartOptions.buildPyramidChartOption(newData) as any
-          );
-          break;
-      }
-    }
-  }
-
-  private toTreeData(data: any) {
-    if (data.length === 1) {
-      const childrenInfo = this.transformTreeData(data[0].children);
-      return {
-        name: data[0].item.text,
-        children: childrenInfo,
-        config: data.config,
-      };
-    }
-
-    const treeInfo = this.transformTreeData(data);
-    return {
-      name: '',
-      children: treeInfo,
-      config: data.config,
-    };
-  }
-
-  private transformTreeData(data: any) {
-    const nodes = [];
-    for (const item of data) {
-      const node: any = {};
-      node.name = item.item.text;
-      if (item.children && item.children.length > 0) {
-        node.children = this.transformTreeData(item.children);
-      }
-      nodes.push(node);
-    }
-    return nodes;
-  }
-
-  private buildRadarChartData(code: any) {
-    const tokens = marked.lexer(code);
-    let items = [];
-    items = MarkdownHelper.markdownToJSON(tokens, items);
-    const currentMap = {
-      id: 'radarchart-' + this.chartIndex,
-      type: 'radarchart',
-      data: items,
-    };
-
-    this.chartInfos.push(currentMap);
-
-    this.chartIndex++;
-    return `<div class="markdown-radarchart ${currentMap.id}"></div>`;
-  }
-
-  private buildGraphvizData(code: any) {
-    this.graphvizIndex++;
-
-    this.graphvizData.push({
-      id: this.graphvizIndex,
-      code,
     });
-
-    return `<div class="graphviz"><svg id="graphviz-${this.graphvizIndex}"></svg></div>`;
-  }
-
-  private renderGraphviz() {
-    for (const graph of this.graphvizData) {
-      const render = dagreD3.render();
-      const g = graphlibDot.read(graph.code);
-
-      d3.select('#graphviz-' + graph.id).call(render, g);
-    }
-  }
-
-  private buildQuadrantChartData(code: any) {
-    const tokens = marked.lexer(code);
-    let items = [];
-    items = MarkdownHelper.markdownToJSON(tokens, items);
-    const currentMap = {
-      id: 'quadrant-' + this.chartIndex,
-      type: 'quadrant',
-      data: items,
-    };
-
-    this.chartInfos.push(currentMap);
-    this.chartIndex++;
-    return `<div class="markdown-quadrant ${currentMap.id}"></div>`;
-  }
-
-  private buildPyramidChartData(code: any) {
-    const tokens = marked.lexer(code);
-    let items = [];
-    items = MarkdownHelper.markdownToJSON(tokens, items);
-    const currentMap = {
-      id: 'pyramid-' + this.chartIndex,
-      type: 'pyramid',
-      data: items,
-    };
-
-    this.chartInfos.push(currentMap);
-    this.chartIndex++;
-    return `<div class="markdown-pyramid ${currentMap.id}"></div>`;
-  }
-
-  private buildTableStep(code: any) {
-    const tokens = marked.lexer(code);
-    let items = [];
-    items = MarkdownHelper.markdownToJSON(tokens, items);
-
-    const maxItem = maxBy(items, (d) => d.children.length);
-    const maxLength = maxItem.children.length;
-
-    let maxWidthClass = '';
-    const MAX_ONE_COLUMN = 10;
-    if (maxLength > MAX_ONE_COLUMN) {
-      maxWidthClass = 'process-step-max';
-    }
-
-    let cols = '';
-    // tslint:disable-next-line:prefer-for-of
-    for (let i = 0; i < items.length; i++) {
-      let itemsStr = '';
-      const title = items[i].item.text;
-      for (let j = 0; j < maxLength; j++) {
-        let text = '';
-        if (items[i].children[j]) {
-          text = items[i].children[j].item.text;
-        }
-        if (text !== '') {
-          itemsStr += `<div class="process-step-item ${maxWidthClass}">${text}</div>`;
-        }
-      }
-
-      cols += `<div class="process-step-column">
-  <div class="process-title">${title}</div><div class="process-body ${
-        maxWidthClass === '' ? '' : 'process-long-body'
-      }">${itemsStr}</div>
-</div>`;
-    }
-
-    return `<div class="markdown-process-step">${cols}</div>`;
-  }
-
-  private buildEchartsData(code: any) {
-    this.echartsIndex++;
-
-    const chartId = 'echarts-' + this.echartsIndex;
-    this.echartsData.push({
-      id: chartId,
-      data: code,
-    });
-
-    return `<div class="normal-echarts" id="${chartId}"></div>`;
-  }
-
-  private renderEcharts() {
-    for (const chartInfo of this.echartsData) {
-      const chartEl = document.getElementById(chartInfo.id);
-      const mychart = echarts.init(chartEl as any);
-      this.chartInstances.push(mychart);
-      mychart.setOption(JSON.parse(chartInfo.data));
-    }
-  }
-
-  private buildToolsets(code: any) {
-    this.toolsetId++;
-
-    const id = 'toolset-' + this.toolsetId;
-    const json = LedgeMarkdownConverter.toJson(code);
-
-    const toolType = json.config.type;
-    this.toolsets.push({
-      id,
-      data: this.getDataByType(json, toolType),
-      type: toolType,
-    });
-
-    return `<div class="toolset-placeholder" id="${id}"></div>`;
-  }
-
-  private getDataByType(
-    json: { tables: any[]; lists: any[]; config: any },
-    type: any
-  ) {
-    switch (type) {
-      case 'slider':
-        return json.lists[0].children;
-      case 'line-chart':
-        return json.tables[0];
-      default:
-        return json;
-    }
   }
 }
