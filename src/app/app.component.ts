a6fe7eede76cdf830f19e6a04e0502492ce69ca0
Phodal Huang <h@phodal.com>
2020-04-12 10:37:26 +0800

    fix: fix header issue for #71

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ledge';
+
+  constructor(private route: Router) {}
+
+  // todo: refactor
+  isHome() {
+    return (
+      this.route.url === '/home' ||
+      this.route.url === '/maturity' ||
+      this.route.url === '/report' ||
+      this.route.url === '/design'
+    );
+  }
 }
