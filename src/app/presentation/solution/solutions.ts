f8444254a6ba7da6f383cbd6e9850135e177ca91
Phodal Huang <h@phodal.com>
2020-04-14 17:37:15 +0800

    docs: add worktile solution

--- src/app/presentation/solution/solutions.ts
+++ src/app/presentation/solution/solutions.ts
@@ -9,4 +9,5 @@ export type Solutions = Array<Solution>;
 export const solutions: Solutions = [
   { displayName: 'Coding', source: 'coding' },
   { displayName: '云开发（腾讯云）', source: 'cloudbase' },
+  { displayName: 'Worktile', source: 'worktile' },
 ];
