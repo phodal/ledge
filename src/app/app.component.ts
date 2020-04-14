9b96d2a57b2f54289d5993b24b83382eefeb20cd
Phodal Huang <h@phodal.com>
2020-04-14 16:13:17 +0800

    feat: add save language logic for i18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+import { StorageMap } from '@ngx-pwa/local-storage';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ledge';
   tran: TranslateService;
 
-  constructor(private route: Router, translate: TranslateService) {
+  constructor(
+    private route: Router,
+    translate: TranslateService,
+    private storage: StorageMap
+  ) {
     translate.setDefaultLang('zh-cn');
     translate.use('zh-cn');
 
@@ -30,5 +35,14 @@ export class AppComponent {
 
   setLanguage(lang: string) {
     this.tran.use(lang);
+    this.storage.set('language', lang);
+  }
+
+  ngOnInit(): void {
+    this.storage.get('language').subscribe((value: string) => {
+      if (!!value) {
+        this.tran.use(value);
+      }
+    });
   }
 }
