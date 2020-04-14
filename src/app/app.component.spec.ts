167bd23b02f2191618a6af1a98c62881d876d64c
Phodal Huang <h@phodal.com>
2020-04-14 16:35:41 +0800

    fix: fix test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,17 +2,29 @@ import { async, TestBed } from '@angular/core/testing';
 import { MatMenuModule } from '@angular/material/menu';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import {
+  TranslateFakeLoader,
+  TranslateLoader,
+  TranslateModule,
+  TranslateService,
+  TranslateStore,
+} from '@ngx-translate/core';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
-        MatMenuModule
+        MatMenuModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useClass: TranslateFakeLoader,
+          },
+        }),
       ],
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
+      providers: [TranslateService, TranslateStore],
     }).compileComponents();
   }));
 
