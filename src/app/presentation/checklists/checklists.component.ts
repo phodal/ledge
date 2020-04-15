5ee459ec0a54cbb15704aebcd4f5f2c2f76bdd04
Phodal Huang <h@phodal.com>
2020-04-15 14:53:02 +0800

    feat: make checklists enable move to tab

--- src/app/presentation/checklists/checklists.component.ts
+++ src/app/presentation/checklists/checklists.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTabChangeEvent } from '@angular/material/tabs/tab-group';
+
 import TECH_CHECKLIST from './checklists/technology.json';
 import PROCESS_CHECKLIST from './checklists/process.json';
 import PEOPLE_CHECKLIST from './checklists/people.json';
 import DOMAIN_CHECKLIST from './checklists/domain.json';
+import { StorageMap } from '@ngx-pwa/local-storage';
 
 @Component({
   selector: 'app-checklists',
@@ -15,7 +18,23 @@ export class ChecklistsComponent implements OnInit {
   peopleChecklist: any = PEOPLE_CHECKLIST;
   domainChecklist: any = DOMAIN_CHECKLIST;
 
-  constructor() {}
+  selectedTabIndex = 0;
 
-  ngOnInit(): void {}
+  constructor(private storage: StorageMap) {}
+
+  ngOnInit(): void {
+    this.storage.get('checklists.last.index').subscribe((value: string) => {
+      console.log(value);
+      if (!!value) {
+        this.selectedTabIndex = parseInt(value, 10);
+      }
+    });
+  }
+
+  onTabChanged($event: MatTabChangeEvent) {
+    this.selectedTabIndex = $event.index;
+    this.storage
+      .set('checklists.last.index', this.selectedTabIndex)
+      .subscribe();
+  }
 }
