8da8a2f74396052ecaa0639d1b8b23654b97afd5
Phodal Huang <h@phodal.com>
2020-04-18 20:39:45 +0800

    docs: init ba think tank

--- src/app/presentation/think-tank/thinktanks.ts
+++ src/app/presentation/think-tank/thinktanks.ts
@@ -8,6 +8,7 @@ export type Thinktanks = Array<Thinktank>;
 
 // todo: 优先级根据内容的质量重新排序。现在的是后来的在后面  + 内容多的在前面，随机组合
 export const thinktanks: Thinktanks = [
+  { displayName: 'BA', source: 'ba', default: true },
   { displayName: 'QA', source: 'qa', default: true },
   { displayName: 'Android', source: 'mobile-android', default: true },
   { displayName: '前端', source: 'frontend', default: true },
