569f9da64dadcf961d3f95b50faf17e5521ee629
Phodal Huang <h@phodal.com>
2020-04-19 19:54:15 +0800

    build: update karma config

--- projects/ledge-view/karma.conf.js
+++ projects/ledge-view/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../coverage/ledge-view'),
+      dir: require('path').join(__dirname, '../../coverage/ledge-vew'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true,
     },
@@ -27,6 +27,12 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu'],
+      },
+    },
     restartOnFileChange: true,
   });
 };
