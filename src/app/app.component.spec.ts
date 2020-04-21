de68cbb671d2dcee329c4f2b6dcf933d605c9566
Phodal Huang <h@phodal.com>
2020-04-21 13:21:27 +0800

    test: add test for app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,6 @@
-import { async, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatMenuModule } from '@angular/material/menu';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
 import {
   TranslateFakeLoader,
   TranslateLoader,
@@ -9,13 +8,24 @@ import {
   TranslateService,
   TranslateStore,
 } from '@ngx-translate/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { CustomMaterialModule } from './shared/custom-material.module';
+import { SharedModule } from './shared/shared.module';
+import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
         MatMenuModule,
+        CustomMaterialModule,
+        BrowserAnimationsModule,
+        SharedModule,
         TranslateModule.forRoot({
           loader: {
             provide: TranslateLoader,
@@ -28,15 +38,33 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'clean-angular'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('ledge');
+  });
+
+  it('should call window.open when click link', () => {
+    spyOn(window, 'open');
+
+    component.openLink('https://devops.phdoal.com');
+
+    expect(window.open).toHaveBeenCalled();
+  });
+
+  it('should enable set language', () => {
+    component.setLanguage('zh-cn');
+
+    expect(component.translate.currentLang).toEqual('zh-cn');
   });
 });
