09c5aa8a011cc0260733af595e81174f0c5d56a4
Phodal Huang <h@phodal.com>
2020-04-21 14:06:00 +0800

    test: add test example for ledge render

--- projects/ledge-render/src/lib/ledge-render.component.spec.ts
+++ projects/ledge-render/src/lib/ledge-render.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { LedgeRenderComponent } from './ledge-render.component';
+import { SimpleChange, SimpleChanges } from '@angular/core';
 
 describe('LedgeChecklistComponent', () => {
   let component: LedgeRenderComponent;
@@ -7,9 +8,9 @@ describe('LedgeChecklistComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LedgeRenderComponent ]
+      declarations: [LedgeRenderComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -20,5 +21,39 @@ describe('LedgeChecklistComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should build paragraph', () => {
+    const content = `这是一个段落`;
+
+    const simpleChange = new SimpleChange('', content, false);
+    const changesObj: SimpleChanges = {
+      content: simpleChange
+    };
+    component.ngOnChanges(changesObj);
+
+    expect(component.markdownData.length).toEqual(1);
+    expect(component.markdownData[0].type).toEqual('paragraph');
+    expect(component.markdownData[0].data).toEqual(content);
+  });
+
+  it('should build checklist', () => {
+    const content = `
+\`\`\`checklist
+- DevOps 检查清单
+  - 文化
+    - 确保跨组织和团队中的业务对齐
+\`\`\`
+`;
+
+    const simpleChange = new SimpleChange('', content, false);
+    const changesObj: SimpleChanges = {
+      content: simpleChange
+    };
+    component.ngOnChanges(changesObj);
+
+    expect(component.markdownData.length).toEqual(1);
+    expect(component.markdownData[0].type).toEqual('checklist');
+    expect(component.markdownData[0].data[0].name).toEqual('DevOps 检查清单');
   });
 });
