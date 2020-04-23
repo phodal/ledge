49dd8b95f627633892ba96f599ba16eeffeaedcc
Phodal Huang <h@phodal.com>
2020-04-23 14:20:15 +0800

    styles: update for mermaid style

--- projects/ledge-render/src/lib/components/ledge-mermaid/ledge-mermaid.component.ts
+++ projects/ledge-render/src/lib/components/ledge-mermaid/ledge-mermaid.component.ts
@@ -10,9 +10,27 @@ export class LedgeMermaidComponent implements OnInit {
   @Input()
   data: string;
   code: string;
-  constructor() {}
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+    mermaid.default.mermaidAPI.initialize({
+      theme: 'default',
+      gantt: {
+        titleTopMargin: 25,
+        barHeight: 48,
+        barGap: 4,
+        topPadding: 50,
+        leftPadding: 75,
+        gridLineStartPadding: 35,
+        fontSize: 18,
+        fontFamily: '"Open-Sans", "sans-serif"',
+        numberSectionStyles: 4,
+        axisFormat: '%Y-%m-%d',
+      }
+    });
+
     mermaid.default.mermaidAPI.render(
       `mermaid-${Math.random().toString(32).slice(2)}`,
       this.data,
