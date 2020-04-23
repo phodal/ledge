7bde871ff1cfd534060a5bcce78e37180ce4a4c4
Phodal Huang <h@phodal.com>
2020-04-23 08:50:43 +0800

    fix: fix data issue for testing

--- projects/ledge-render/src/lib/components/ledge-mermaid/ledge-mermaid.component.spec.ts
+++ projects/ledge-render/src/lib/components/ledge-mermaid/ledge-mermaid.component.spec.ts
@@ -16,6 +16,32 @@ describe('LedgeMermaidComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LedgeMermaidComponent);
     component = fixture.componentInstance;
+    component.data = `
+\`\`\`mermaid
+gantt
+\ttitle  双周迭代模型
+\tdateFormat YYYY-MM-DD
+  axisFormat W%U
+
+\tsection A 组
+\t需求评审排期  :a1, 2020-01-05, 7d
+\t提供 UI 素材 :a2, after a1 ,7d
+\tAPI 开发    :a3, after a1 , 7d
+\t第一周开始   :done, a4, after a3 , 7d
+\t第二周开始   :done, a5, after a4 , 7d
+\t提测        :crit, a6, after a5 , 7d
+  灰度全量     :crit, a7, after a6 , 3d
+
+\tsection B 组
+\t需求评审排期  :b1, 2020-01-19, 7d
+\t提供 UI 素材 :b2, after b1 ,7d
+\tAPI 开发    :b3, after b1 , 7d
+\t第一周开始   :done, b4, after b3 , 7d
+\t第二周开始   :done, b5, after b4 , 7d
+\t提测        :crit, b6, after b5 , 7d
+  灰度全量     :crit, b7, after b6 , 3d
+\`\`\`
+    `;
     fixture.detectChanges();
   });
 
