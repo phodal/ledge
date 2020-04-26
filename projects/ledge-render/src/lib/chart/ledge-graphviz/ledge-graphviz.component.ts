6469d4437669395daee4a634015f3d3dce409cbc
Phodal Huang <h@phodal.com>
2020-04-26 21:15:42 +0800

    feat: add zoom for skilltree for #168

--- projects/ledge-render/src/lib/chart/ledge-graphviz/ledge-graphviz.component.ts
+++ projects/ledge-render/src/lib/chart/ledge-graphviz/ledge-graphviz.component.ts
@@ -1,11 +1,4 @@
-import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  Input,
-  OnInit,
-  ViewChild
-} from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import * as d3 from 'd3';
 import * as dagreD3 from 'dagre-d3';
 import * as graphlibDot from 'graphlib-dot';
@@ -15,20 +8,25 @@ import * as graphlibDot from 'graphlib-dot';
   templateUrl: './ledge-graphviz.component.html',
   styleUrls: ['./ledge-graphviz.component.scss'],
 })
-export class LedgeGraphvizComponent implements OnInit, AfterViewInit {
+export class LedgeGraphvizComponent implements AfterViewInit {
   @Input()
   data: string;
 
   @ViewChild('chart', {}) reporter: ElementRef;
 
-  ngOnInit(): void {}
-
   ngAfterViewInit(): void {
-    const render = dagreD3.render();
-    const g = graphlibDot.read(this.data);
-
     setTimeout(() => {
-      d3.select(this.reporter.nativeElement).select('svg').call(render, g);
+      const rootSvg = d3.select(this.reporter.nativeElement).select('svg');
+      const inner = d3.select(this.reporter.nativeElement).select('svg g');
+      const zoom = d3.zoom().on('zoom', () => {
+          inner.attr('transform', d3.event.transform);
+        });
+
+      rootSvg.call(zoom);
+
+      const render = dagreD3.render();
+      const g = graphlibDot.read(this.data);
+      d3.select(this.reporter.nativeElement).select('svg g').call(render, g);
     });
   }
 }
