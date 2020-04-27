4aef4bf083f298b72f38c044201b113b01dce708
Phodal Huang <h@phodal.com>
2020-04-27 11:28:00 +0800

    fix: fix test

--- projects/ledge-render/src/lib/chart/ledge-sunburst/ledge-sunburst.component.spec.ts
+++ projects/ledge-render/src/lib/chart/ledge-sunburst/ledge-sunburst.component.spec.ts
@@ -16,6 +16,19 @@ describe('LedgeSunburstComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LedgeSunburstComponent);
     component = fixture.componentInstance;
+    component.data = [{
+      children: [
+        {
+          children: [
+            {
+              children: [],
+              name: '',
+            },
+          ],
+          name: '',
+        },
+      ],
+    }];
     fixture.detectChanges();
   });
 
