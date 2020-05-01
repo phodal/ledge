4993f3d60886f691b40570e948f4fe8cb9e67ca4
Phodal Huang <h@phodal.com>
2020-05-01 10:20:54 +0800

    testingg: add lost test for markdown render

--- src/app/features/markdown-render/markdown-render.component.spec.ts
+++ src/app/features/markdown-render/markdown-render.component.spec.ts
@@ -4,7 +4,7 @@ import { MarkdownRenderComponent } from './markdown-render.component';
 import { SharedModule } from '../../shared/shared.module';
 import { RouterTestingModule } from '@angular/router/testing';
 
-describe('MarkdownRenderComponent', () => {
+fdescribe('MarkdownRenderComponent', () => {
   let component: MarkdownRenderComponent;
   let fixture: ComponentFixture<MarkdownRenderComponent>;
 
@@ -18,11 +18,42 @@ describe('MarkdownRenderComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(MarkdownRenderComponent);
     component = fixture.componentInstance;
-    component.data = '/assets/docs/manual.md';
+    component.data = `
+# h1
+
+## h2
+
+[link](link)
+
+`;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should build toc str', () => {
+    expect(component.tocStr)
+      .toEqual(`<div class="level_1" data-tocId="0"><a id="menu-h1" href="#h1" title=h1>h1</a>
+<div class="level_child"><div class="level_2" data-tocId="1"><a id="menu-h2" href="#h2" title=h2>h2</a></div></div>
+</div>`);
+  });
+
+  it('should scroll to top when call scrollToTop', () => {
+    component.sticky = true;
+    const scrollTop = window.scrollTo;
+    window.scrollTo = jasmine.createSpy('scroll');
+
+    component.scrollToTop();
+    expect(component.sticky).toBeFalse();
+    expect(window.scrollTo).toHaveBeenCalled();
+
+    window.scrollTo = scrollTop;
+  });
+
+  it('should not show sticky button when not scroll', () => {
+    component.handleScroll({});
+    expect(component.sticky).toBeFalse();
   });
 });
