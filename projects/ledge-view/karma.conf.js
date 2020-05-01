1d7f8400f02d32bcbcf363e6e7b2705969e808f4
Phodal Huang <h@phodal.com>
2020-05-01 10:10:09 +0800

    fix: fix view config issue

--- projects/ledge-view/karma.conf.js
+++ projects/ledge-view/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../coverage/ledge-vew'),
+      dir: require('path').join(__dirname, '../../coverage/ledge-view'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true,
     },
