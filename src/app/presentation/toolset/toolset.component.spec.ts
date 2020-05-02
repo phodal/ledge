2b11423dded072202b3c37e54ead596770616272
Phodal Huang <h@phodal.com>
2020-05-02 09:45:15 +0800

    refactor: modify toolset order

--- src/app/presentation/toolset/toolset.component.spec.ts
+++ src/app/presentation/toolset/toolset.component.spec.ts
@@ -1,10 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { LedgeRenderModule } from '@ledge-framework/render';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { ToolsetComponent } from './toolset.component';
 import { SharedModule } from '../../shared/shared.module';
 import { CustomMaterialModule } from '../../shared/custom-material.module';
-import { LedgeRenderModule } from '@ledge-framework/render';
-import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ToolSetComponent', () => {
   let component: ToolsetComponent;
