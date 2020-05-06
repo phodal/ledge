604610987d25d3b7491fad9c1d0f620e18c9436e
Phodal Huang <h@phodal.com>
2020-05-06 11:33:54 +0800

    build: fix render import issue

--- src/app/shared/components/ledge-multiple-docs/ledge-multiple-docs.component.spec.ts
+++ src/app/shared/components/ledge-multiple-docs/ledge-multiple-docs.component.spec.ts
@@ -1,13 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { LedgeMultipleDocsComponent } from './ledge-multiple-docs.component';
 import { HttpClientModule } from '@angular/common/http';
-import { SharedModule } from '../../shared.module';
 import { RouterTestingModule } from '@angular/router/testing';
-import { LedgeRenderModule } from 'ledge-render';
-import { CustomMaterialModule } from '../../custom-material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserTestingModule } from '@angular/platform-browser/testing';
+import { LedgeRenderModule } from '@ledge-framework/render';
+
+import { CustomMaterialModule } from '../../custom-material.module';
+import { SharedModule } from '../../shared.module';
+import { LedgeMultipleDocsComponent } from './ledge-multiple-docs.component';
 
 describe('LedgeMultipleDocsComponent', () => {
   let component: LedgeMultipleDocsComponent;
