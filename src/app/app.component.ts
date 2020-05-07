c36007ae24c540f9e911c5fe40f2d5209205dfbb
Phodal Huang <h@phodal.com>
2020-05-07 11:08:24 +0800

    feat(intro): import intro.js for #194

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { StorageMap } from '@ngx-pwa/local-storage';
+import * as introJs from 'intro.js/intro.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'ledge';
+  introJS = introJs();
 
   constructor(
     private route: Router,
@@ -40,6 +42,41 @@ export class AppComponent implements OnInit {
         this.translate.use(value);
       }
     });
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.startIntro();
+    }, 3000);
+  }
+
+  private startIntro() {
+    this.introJS.setOptions({
+      steps: [
+        {
+          element: '#step1',
+          intro: 'Welcome to your new app!',
+          position: 'bottom',
+        },
+        {
+          element: '#step2',
+          intro: "Ok, wasn't that fun?",
+          position: 'right',
+        },
+        {
+          element: '#step3',
+          intro: "let's keep going",
+          position: 'top',
+        },
+        {
+          element: '#step-period',
+          intro: 'More features, more fun.',
+          position: 'right',
+        },
+      ],
+      showProgress: true,
+    });
+    this.introJS.start();
   }
 
   openLink(link: string) {
