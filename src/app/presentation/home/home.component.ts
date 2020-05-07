7e137dbb6469933d165580b8a811788d871b6ff3
Phodal Huang <h@phodal.com>
2020-05-07 14:08:03 +0800

    feat(intro): add basic intro for ledge && closed #194

--- src/app/presentation/home/home.component.ts
+++ src/app/presentation/home/home.component.ts
@@ -85,16 +85,88 @@ config: {"rowHeight": "350px", "colors": [{"bg":"#e55852","font":"#b71a09"},{"bg
     },
   ];
 
-  private firstStep = {
-    title: '欢迎来到 Ledge 知识平台，开启你的提升之旅',
-    text: `Ledge （源于 Know-Ledge，意指承载物），它包含了各种最佳实践、原则与模式、实施手册、度量、工具，用于帮助您的企业在数字化时代更好地前进，还有 DevOps 转型。`,
-    attachTo: {
-      element: '.ledge-title',
-      on: 'bottom',
+  steps = [
+    {
+      title: '欢迎来到 Ledge 知识平台，开启你的提升之旅',
+      text: `Ledge （源于 Know-Ledge，意指承载物） 是一个开源的 DevOps 知识平台，它包含了各种最佳实践、原则与模式、实施手册、度量、工具，用于帮助您的企业在数字化时代更好地前进，还有 DevOps 转型。`,
+      attachTo: {
+        element: '.ledge-title',
+        on: 'bottom',
+      },
+      buttons: this.defaultButtons,
+      id: 'creating',
     },
-    buttons: this.defaultButtons,
-    id: 'creating',
-  };
+    {
+      title: '元素周期表',
+      text: `『元素周期表』是我们整理了常用的 DevOps 流程中的工具产出的一个工具，用于帮助你设计和指导 DevOps 流程设计与实施。`,
+      attachTo: {
+        element: '.intro-periodic',
+        on: 'top',
+      },
+      buttons: this.defaultButtons,
+      id: 'pattern',
+    },
+    {
+      title: '案例学习',
+      text: `在技术社区这个丰富的知识库中，我们总结了传统企业走向 DevOps 的经验，并浓缩到易于使用的内容和材料中。`,
+      attachTo: {
+        element: '.intro-case',
+        on: 'top',
+      },
+      buttons: this.defaultButtons,
+      id: 'case',
+    },
+    {
+      title: '模型与原则',
+      text: `『模型与原则』包含了各种实践背后的思想和模式，用于指导 DevOps 教练、技术人员、管理者实践 DevOps 和对应的实践，如流畅度模型、学习型组织等。`,
+      attachTo: {
+        element: '.intro-pattern',
+        on: 'bottom',
+      },
+      buttons: this.defaultButtons,
+      id: 'pattern',
+    },
+    {
+      title: '最佳实践',
+      text: `『最佳实践』是 Ledge 的精华所在，它提炼了一系列的最佳、最优软件开发实践，用于帮助你完善自身的知识体系。`,
+      attachTo: {
+        element: '.intro-practise',
+        on: 'bottom',
+      },
+      buttons: this.defaultButtons,
+      id: 'practise',
+    },
+    {
+      title: '实施手册',
+      text: `『实施手册』是一个完整的 DevOps 实施指导手册，用于手把手带你进入 DevOps 世界。`,
+      attachTo: {
+        element: '.intro-manual',
+        on: 'bottom',
+      },
+      buttons: this.defaultButtons,
+      id: 'manual',
+    },
+    {
+      title: '工具',
+      text: `除了，将常用的工具提炼到这个项目中。我们还将 Ledge 中一系列的模式和原则，提取成工具放置到这个项目中。`,
+      attachTo: {
+        element: '.intro-tool',
+        on: 'bottom',
+      },
+      buttons: this.defaultButtons,
+      id: 'tool',
+    },
+    {
+      title: '开源 + 开放 + 协作 + 透明',
+      text: `如果你在使用过程中，遇到任何问题或者对我们有任何意见，欢迎向我们提出反馈！让我们一起成长！`,
+      attachTo: {
+        element: '.home-link',
+        on: 'bottom',
+      },
+      buttons: this.defaultButtons,
+      id: 'github',
+    },
+  ];
 
   setCurrentAtomCategory(category: string) {
     this.category = category;
@@ -121,8 +193,14 @@ config: {"rowHeight": "350px", "colors": [{"bg":"#e55852","font":"#b71a09"},{"bg
     };
     this.shepherdService.modal = true;
     this.shepherdService.confirmCancel = false;
-    this.shepherdService.addSteps([this.firstStep]);
-    this.shepherdService.start();
+    this.shepherdService.addSteps(this.steps);
+
+    this.storage.get('intro.hadDone').subscribe((value: boolean) => {
+      if (!value) {
+        this.shepherdService.start();
+        this.storage.set('intro.hadDone', true).subscribe(() => {});
+      }
+    });
   }
 
   show(event: Partial<IntersectionObserverEntry>) {
