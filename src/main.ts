b8ce5394e2804c363b72f7fddc3fe3408421e06a
LiuuY <mail4c59@gmail.com>
2020-05-07 22:46:24 +0800

    refactor: use turborlink to speedup menu change

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import Turbolinks from 'turbolinks';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
+
+  Turbolinks.start();
+
+  document.addEventListener('turbolinks:load', () => {
+    platformBrowserDynamic()
+      .bootstrapModule(AppModule)
+      .catch((err) => console.error(err));
+  });
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
