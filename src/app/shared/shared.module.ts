b70a608543df9da5c4f39f6746f033cb4c2f5e8b
Phodal Huang <h@phodal.com>
2020-05-08 19:50:09 +0800

    refactor: clean old code

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,9 +6,7 @@ import { RouterModule } from '@angular/router';
 import { LedgeRenderModule } from '@ledge-framework/render';
 import { TranslateModule } from '@ngx-translate/core';
 import { MarkdownModule, MarkedOptions } from 'ngx-markdown';
-import { MarkdownRatingItemComponent } from '../features/rating-radar-chart/markdown-rating-item/markdown-rating-item.component';
-import { MarkdownRatingComponent } from '../features/rating-radar-chart/markdown-rating/markdown-rating.component';
-import { RatingRadarChartComponent } from '../features/rating-radar-chart/rating-radar-chart.component';
+
 import { CustomMaterialModule } from './custom-material.module';
 import { LedgeMarkdownRenderComponent } from './components/ledge-markdown-render/ledge-markdown-render.component';
 import { LedgeMultipleDocsComponent } from './components/ledge-multiple-docs/ledge-multiple-docs.component';
@@ -41,23 +39,9 @@ import { LedgeMultipleDocsComponent } from './components/ledge-multiple-docs/led
       },
     }),
   ],
-  declarations: [
-    LedgeMarkdownRenderComponent,
-    LedgeMultipleDocsComponent,
-
-    MarkdownRatingComponent,
-    MarkdownRatingItemComponent,
-    RatingRadarChartComponent,
-  ],
+  declarations: [LedgeMarkdownRenderComponent, LedgeMultipleDocsComponent],
   providers: [],
-  exports: [
-    LedgeMarkdownRenderComponent,
-    LedgeMultipleDocsComponent,
-
-    MarkdownRatingComponent,
-    MarkdownRatingItemComponent,
-    RatingRadarChartComponent,
-  ],
+  exports: [LedgeMarkdownRenderComponent, LedgeMultipleDocsComponent],
   entryComponents: [],
 })
 export class SharedModule {}
